f2910852d30c1cc0ffa23d8f30dbe1190cbe6a8f
David Fuentes <davidf0827@gmail.com>
2020-08-12 00:34:11 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,22 @@
 class Post 
- 
+ attr_accessor :author, :title 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def artist_name 
+    if self.artist
+      self.artist.name
+    else 
+      nil
+    end
+  end 
+  
+  def self.all 
+    @@all 
+  end
 end
